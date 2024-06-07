12058819b77f0f39dd6d8aebfd6d3ec52eb04d6c
ilevillani <46748858+ilevillani@users.noreply.github.com>
2024-06-07 17:40:35 +0200

    Update Facoltativo_a.c

--- Facoltativo_a.c
+++ Facoltativo_a.c
@@ -6,7 +6,7 @@
 */
 
 int main() {
-    float primo_numero;
+    	float primo_numero;
 	float secondo_numero;
 	float terzo_numero;
 	float somma;
@@ -17,8 +17,8 @@ int main() {
 	printf("Ricorda che, per separare le cifre decimali, dovrai utilizzare un punto e non una virgola. Es.: 52.18 \f\n");
 	
 	//Le seguenti righe di codice prendono l'input dall'utente e lo salvano nelle variabili
-    printf("Inserisci il primo numero: ");
-    scanf("%f", &primo_numero);
+    	printf("Inserisci il primo numero: ");
+    	scanf("%f", &primo_numero);
 	printf("Inserisci il secondo numero: ");
 	scanf("%f", &secondo_numero);
 	printf("Inserisci il terzo numero: ");
@@ -30,13 +30,13 @@ int main() {
 	somma = primo_numero + secondo_numero + terzo_numero;
 	
 	//Calcola la media
-    media = somma / 3;
+   	media = somma / 3;
 
-    //Stampa la media con due cifre decimali
-    printf("La media aritmetica con due cifre decimali è: %.2f\f\n", media);
+   	//Stampa la media con due cifre decimali
+   	printf("La media aritmetica con due cifre decimali è: %.2f\f\n", media);
 
-    //Stampa la media arrotondata senza cifre decimali
-    printf("La media aritmetica arrotondata è: %.0f\n", media);
+   	//Stampa la media arrotondata senza cifre decimali
+    	printf("La media aritmetica arrotondata è: %.0f\n", media);
 
-    return 0;
+	return 0;
 }
